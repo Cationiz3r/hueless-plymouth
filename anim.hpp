7a410c5b40c226d8cd704d0b35747df5f1a3e99b
Martin Cation <martin cation et protonmail dot com>
2022-06-29 09:14:21 +0700

    removed lines

--- anim.hpp
+++ anim.hpp
@@ -23,17 +23,6 @@ class Anim {
 	sf::Vertex triangle[3][4];
 	void tri_render();
 
-	// Lines config
-	std::vector<int> line_lengths; // i:Probabilty for length i+1
-	int line_types; // 1/n Probability for start of lines block
-	float line_spacing, line_width, line_unit;
-	int line_max_horz, line_max_vert;
-	// Lines
-	std::vector<std::vector<int>> lines; // Hardcoded range
-	std::vector<int> line_frames; // frames after which line is drawn
-	void line_gen();
-	void line_render();
-
 	// Math
 	sf::Vector2f mid(sf::Vector2f A, sf::Vector2f B, float factor);
 public:
