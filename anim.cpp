142937c30966121959b15d452308e346f28a2f0b
Martin Cation <martin cation et protonmail dot com>
2022-06-29 13:19:34 +0700

    pefected end animation for triangle

--- anim.cpp
+++ anim.cpp
@@ -8,59 +8,82 @@ Anim::Anim() {
 	window->setVerticalSyncEnabled(0);
 
 	tri_size = 150;
-	tri_width = 20;
-	tri_duration = 100;
+	tri_width = 30;
+	tri_duration = 300; //Loop
 	tri_duration_end = 150;
+	trail_endlength = 5;
 
-	frame_last = tri_duration*3 + tri_duration_end;
+	frame_last = tri_duration + tri_duration_end;
 }
 
 void Anim::tri_render() {
 	sf::Vector2f center = { (float)window->getSize().x/2, (float)window->getSize().y/2 };
 
-	float angle = PI/2,
-		factor = 1 - (frame % tri_duration) / (float)tri_duration,
-		side = (frame / tri_duration) % 3; // This means it take 3 "tri_duration" to complete a cycle
-	if (frame >= tri_duration*3) angle += (1 - std::cos((frame-tri_duration*3)/(float)tri_duration_end*PI))/2*TRI;
-	triangle[0][0].color = sf::Color(0, 0, 0);
-	triangle[0][0].position = center + mid(
-		sf::Vector2f(tri_size * std::cos(angle+side*TRI),     tri_size * std::sin(angle+side*TRI)),
-		sf::Vector2f(tri_size * std::cos(angle+(side+1)*TRI), tri_size * std::sin(angle+(side+1)*TRI)),
-		factor
-	);
-	triangle[0][1].color = sf::Color(0, 0, 0);
-	triangle[0][1].position = center + mid(
-		sf::Vector2f((tri_size-tri_width) * std::cos(angle+side*TRI),     (tri_size-tri_width) * std::sin(angle+side*TRI)),
-		sf::Vector2f((tri_size-tri_width) * std::cos(angle+(side+1)*TRI), (tri_size-tri_width) * std::sin(angle+(side+1)*TRI)),
-		factor
-	);
-	triangle[2][3].color = sf::Color(255, 255, 255);
-	triangle[2][3].position = center + mid(
-		sf::Vector2f(tri_size * std::cos(angle+(side+2)*TRI), tri_size * std::sin(angle+(side+2)*TRI)),
-		sf::Vector2f(tri_size * std::cos(angle+side*TRI),     tri_size * std::sin(angle+side*TRI)),
-		factor
-	);
-	triangle[2][2].color = sf::Color(255, 255, 255);
-	triangle[2][2].position = center + mid(
-		sf::Vector2f((tri_size-tri_width) * std::cos(angle+(side+2)*TRI), (tri_size-tri_width) * std::sin(angle+(side+2)*TRI)),
-		sf::Vector2f((tri_size-tri_width) * std::cos(angle+side*TRI),       (tri_size-tri_width) * std::sin(angle+side*TRI)),
-		factor
-	);
-	for (int i=1; i<3; ++i) {
-		int c = 255*(i+factor-1)/2;
-		triangle[i][0].color = triangle[(i+2)%3][3].color =
-		triangle[i][1].color = triangle[(i+2)%3][2].color = sf::Color(c, c, c);
-		triangle[i][0].position = triangle[(i+2)%3][3].position = center + sf::Vector2f(
-			tri_size * std::cos(angle + (i+side)*TRI),
-			tri_size * std::sin(angle + (i+side)*TRI)
-		);
-		triangle[i][1].position = triangle[(i+2)%3][2].position = center + sf::Vector2f(
-			(tri_size-tri_width) * std::cos(angle + (i+side)*TRI),
-			(tri_size-tri_width) * std::sin(angle + (i+side)*TRI)
-		);
+	float angle = PI/2;
+	float offset = 1 - frame % (tri_duration/3) / (tri_duration/3.0);
+	float offset_tail = offset;
+	int step = (frame / (tri_duration/3)) % 3; // Rotated everything by 120 deg
+	float trail_length = 2;
+	int n = 3;
+
+	// End of loop, finish animation
+	if (frame > tri_duration) {
+		//angle += (1 - std::cos((frame-tri_duration)/(float)tri_duration_end*PI))/2*TRI;
+		trail_length += (trail_endlength - trail_length)*std::pow((frame-tri_duration)/(float)tri_duration_end, 2);
+		offset_tail = trail_length - offset;
+		--n;
+		while (offset_tail > 1) {
+			offset_tail -= 1;
+			n += 1;
+		}
+		offset_tail = 1 - offset_tail;
 	}
 
-	for (int i=0; i<3; ++i) window->draw(triangle[i], 4, sf::Quads);
+	//int n = std::floor(trail_length) + 1; // Number of segments
+	std::vector<sf::Vertex[4]> trail(n); // Each segment is a quad
+
+	// Begin is darkest, End is brightest
+	// Begin: 0    End:  3  <-- outer side
+	//         1        2   <-- inner side
+	trail[0  ][0].color = trail[0  ][1].color = sf::Color(0, 0, 0);
+	trail[n-1][3].color = trail[n-1][2].color = sf::Color(255, 255, 255);
+
+	// Begin & End has position between corners based on offset
+	float tri_size_inner = tri_size - tri_width; // Avoid recalculation
+	trail[0][0].position = center + sf::Vector2f(
+		mid(tri_size       * std::cos(angle + TRI*(step)),     tri_size       * std::cos(angle + TRI*(step+1)), offset),
+		mid(tri_size       * std::sin(angle + TRI*(step)),     tri_size       * std::sin(angle + TRI*(step+1)), offset));
+	trail[0][1].position = center + sf::Vector2f(
+		mid(tri_size_inner * std::cos(angle + TRI*(step)),     tri_size_inner * std::cos(angle + TRI*(step+1)), offset),
+		mid(tri_size_inner * std::sin(angle + TRI*(step)),     tri_size_inner * std::sin(angle + TRI*(step+1)), offset));
+	trail[n-1][3].position = center + sf::Vector2f(
+		mid(tri_size       * std::cos(angle + TRI*(step+n-1)), tri_size       * std::cos(angle + TRI*(step+n)), offset_tail),
+		mid(tri_size       * std::sin(angle + TRI*(step+n-1)), tri_size       * std::sin(angle + TRI*(step+n)), offset_tail));
+	trail[n-1][2].position = center + sf::Vector2f(
+		mid(tri_size_inner * std::cos(angle + TRI*(step+n-1)), tri_size_inner * std::cos(angle + TRI*(step+n)), offset_tail),
+		mid(tri_size_inner * std::sin(angle + TRI*(step+n-1)), tri_size_inner * std::sin(angle + TRI*(step+n)), offset_tail));
+
+	for (int i=1; i<n; ++i) {
+		int c = 255*(i+offset-1)/trail_length;
+		// Making everything full bright when end goes over start
+		//   then delay that frame for a while (post-this)
+		if (trail_length > 3) c += (trail_length-3)/(trail_endlength-3)*(255-c);
+
+		// Last one's end color is this one's begin color (gradient)
+		trail[i][0].color = trail[(i+n-1)%n][3].color =
+		trail[i][1].color = trail[(i+n-1)%n][2].color = sf::Color(c, c, c);
+
+		// Same for position
+		trail[i][0].position = trail[(i+n-1)%n][3].position = center + sf::Vector2f(
+			tri_size       * std::cos(angle + TRI*(i+step)),
+			tri_size       * std::sin(angle + TRI*(i+step)));
+		trail[i][1].position = trail[(i+n-1)%n][2].position = center + sf::Vector2f(
+			tri_size_inner * std::cos(angle + TRI*(i+step)),
+			tri_size_inner * std::sin(angle + TRI*(i+step)));
+	}
+
+	//std::cout <<frame <<": " <<n <<" " <<trail_length <<" " <<offset <<" " <<offset_tail <<"\n";
+	for (auto t: trail) window->draw(t, 4, sf::Quads);
 }
 
 void Anim::saveframe() {
@@ -95,10 +118,6 @@ void Anim::play() {
 	}
 }
 
-
-sf::Vector2f Anim::mid(sf::Vector2f A, sf::Vector2f B, float factor) {
-	return sf::Vector2f(
-		A.x*factor + B.x*(1-factor),
-		A.y*factor + B.y*(1-factor)
-	);
+float Anim::mid(float A, float B, float factor) {
+	return A*factor + B*(1-factor);
 }
--- anim.hpp
+++ anim.hpp
@@ -9,6 +9,7 @@
 
 #define PI 3.141592
 #define TRI PI*2/3
+//#define SIDES 3
 
 class Anim {
 	int frame, frame_last;
@@ -18,12 +19,13 @@ class Anim {
 	// Triangle config
 	float tri_size, tri_width;
 	int tri_duration, tri_duration_end;
+	float trail_endlength;
 	// Middle Triangle
-	sf::Vertex triangle[3][4];
 	void tri_render();
 
 	// Math
 	sf::Vector2f mid(sf::Vector2f A, sf::Vector2f B, float factor);
+	float mid(float A, float B, float factor);
 public:
 	Anim();
 	void play();
